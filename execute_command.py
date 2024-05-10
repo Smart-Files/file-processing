5a96d504a380b0c11251e84a8b14fef0d67d57ca
NeagDolph <neil@manymailz.com>
2024-05-10 03:38:33 -0400

    changes to smartfiles tools bot

--- execute_command.py
+++ execute_command.py
@@ -1,33 +1,69 @@
-def execute_command(command: str) -> dict["stdout": str, "stderr": str]:
+import subprocess
+import shlex
+import os
+import re
+
+def get_current_directory_contents():
+    current_directory = os.getcwd()
+    contents = os.listdir(current_directory)
+    return contents
+
+def execute_command(command: str, working_dir="working_dir") -> dict["stdout": str, "stderr": str]:
     """Executes a command in the shell and returns the output.
 
     Args:
     - command: str - The command to execute in the shell.
+
+    Returns:
+    - output: str - The output of the command.
     """
 
-
-    print("start-finish:", command[0], command[len(command)-1])
     command = command.strip()
     if command[0] in ["'", "`", '"', '`'] and command[len(command)-1] == command[0]:
         command = command[1:len(command)-1]
 
-    args = shlex.split(command)
-    print("ARGS: ", args)
-    result = subprocess.run(args, stdout=subprocess.PIPE)
+    # Change the current working directory to the specified directory
+    cwd = os.getcwd()
+    os.chdir(working_dir)
+     
+    result = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,  # Capture stderr as well
+        text=True,  # Decode the output to strings
+        shell=True
+    )
+
+    print("Command execution completed.")
+    print("Return Code:", result.returncode)
+    print("Standard Output:", result.stdout)
+    print("Standard Error:", result.stderr)
 
     print("RESULT: ", result)
 
     stdout = None
     if (result.stdout):
-        stdout = result.stdout.decode('utf-8')
+        stdout = result.stdout
 
     stderr = None
     if (result.stderr):
-        stderr = result.stderr.decode('utf-8')
-    
-    output = {"stdout": stdout, "stderr": stderr}
+        stderr = result.stderr
+
+    output = f"""Command: {command}
+
+    STDOUT: 
+    {stdout}
+
+    STDERR:
+    {stderr}
+
+    Return Code: {result.returncode}
+
+    Current Directory Contents:
+    {get_current_directory_contents()}
+    """
+
+
+    os.chdir(cwd)
     print(output)
     return output
-
-
-print(execute_command("ls -l"))
--- tools_agent.py
+++ tools_agent.py
@@ -5,13 +5,12 @@ from langsmith.wrappers import wrap_openai
 from langsmith import traceable
 from langchain_core.tools import tool, Tool
 from langchain import hub
-from langchain.agents import AgentExecutor, create_tool_calling_agent, AgentType, initialize_agent, load_tools
+from langchain.agents import AgentExecutor, create_tool_calling_agent, AgentType, initialize_agent, load_tools, create_react_agent
 import subprocess
 from langchain.document_loaders.markdown import UnstructuredMarkdownLoader
 from langchain_community.document_loaders import BSHTMLLoader
 from langchain.document_loaders.text import TextLoader
-from langchain.document_loaders.pdf import BasePDFLoader
-from langchain.document_loaders.html import UnstructuredHTMLLoader
+from langchain_community.document_loaders.pdf import BasePDFLoader
 from langchain_chroma import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.globals import set_debug, set_verbose
@@ -28,13 +27,17 @@ from langchain_core.runnables import (
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
-from langchain.tools.retriever import create_retriever_tool
 
 from langsmith import Client
+
+from execute_command import execute_command
+
+from tool_doc_retrieval import create_file_retrieval_tool
 
 
 import os
 from dotenv import load_dotenv
+from langchain_core.callbacks import BaseCallbackHandler, BaseCallbackManager
 
 
 # chroma_client = Chroma.PersistentClient()
@@ -45,7 +48,7 @@ set_debug(True)
 load_dotenv()
 PERSIST_DIR = 'db'
 
-OPENROUTER_API_KEY = os.getenv('OPENROUTER_API_KEY')
+OPENROUTER_API_KEY = os.getenv('GROQ_API_KEY')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 
@@ -55,17 +58,20 @@ LANGCHAIN_API_KEY= os.getenv('LANGSMITH_API_KEY')
 LANGCHAIN_PROJECT="smartfile"
 
 MODEL_CODELLAMA = "phind/phind-codellama-34b"
-MODEL_LLAMA3 = "meta-llama/llama-3-70b-instruct:nitro"
+MODEL_LLAMA3 = "llama3-70b-8192"
 MODEL_LLAMA3_8B = "meta-llama/llama-3-8b-instruct:nitro"
 MODEL_MIXTRAL22 = "mistralai/mixtral-8x22b-instruct"
 MODEL_PALM2 = "google/palm-2-codechat-bison-32k"
 MODEL_QWEN = "qwen/qwen-110b-chat"
 
+API_BASE = "https://api.groq.com/openai/v1"
+# API_BASE = "https://openrouter.ai/api/v1"
+# 
 # Initliaze models
-llm_llama3 = ChatOpenAI(openai_api_key=OPENROUTER_API_KEY, openai_api_base="https://openrouter.ai/api/v1", model_name=MODEL_LLAMA3)
-llm_llama3_8b = ChatOpenAI(openai_api_key=OPENROUTER_API_KEY, openai_api_base="https://openrouter.ai/api/v1", model_name=MODEL_LLAMA3_8B)
-llm_codellama = ChatOpenAI(openai_api_key=OPENROUTER_API_KEY, openai_api_base="https://openrouter.ai/api/v1", model_name=MODEL_CODELLAMA)
-llm_mixtral22 = ChatOpenAI(openai_api_key=OPENROUTER_API_KEY, openai_api_base="https://openrouter.ai/api/v1", model_name=MODEL_MIXTRAL22)
+llm_llama3 = ChatOpenAI(openai_api_key=OPENROUTER_API_KEY, openai_api_base=API_BASE, model_name=MODEL_LLAMA3)
+# llm_llama3_8b = ChatOpenAI(openai_api_key=OPENROUTER_API_KEY, openai_api_base=API_BASE, model_name=MODEL_LLAMA3_8B)
+# llm_codellama = ChatOpenAI(openai_api_key=OPENROUTER_API_KEY, openai_api_base=API_BASE, model_name=MODEL_CODELLAMA)
+# llm_mixtral22 = ChatOpenAI(openai_api_key=OPENROUTER_API_KEY, openai_api_base=API_BASE, model_name=MODEL_MIXTRAL22)
 
 client = Client()
 
@@ -97,7 +103,7 @@ def load_documents_db(directory: str):
         document = loader.load()
         documents.extend(document)
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=0)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=200)
     texts = text_splitter.split_documents(documents)
 
     vectordb = Chroma.from_documents(documents=texts, embedding=embeddings, persist_directory=PERSIST_DIR)
@@ -108,87 +114,36 @@ def load_documents_db(directory: str):
 
     return vectordb
 
-@tool
-def execute_command(command: str) -> dict["stdout": str, "stderr": str]:
-    """Executes a command in the shell and returns the output.
-
-    Args:
-    - command: str - The command to execute in the shell.
-    """
-
-
-    print("start-finish:", command[0], command[len(command)-1])
-    command = command.strip()
-    if command[0] in ["'", "`", '"', '`'] and command[len(command)-1] == command[0]:
-        command = command[1:len(command)-1]
-
-    args = shlex.split(command)
-    print("ARGS: ", args)
-    result = subprocess.run(args, stdout=subprocess.PIPE)
-
-    print("RESULT: ", result)
-
-    stdout = None
-    if (result.stdout):
-        stdout = result.stdout.decode('utf-8')
-
-    stderr = None
-    if (result.stderr):
-        stderr = result.stderr.decode('utf-8')
     
-    output = {"stdout": stdout, "stderr": stderr}
-    print(output)
-    return output
-
-
-
-
 
 def init_tools_agent(llm, input, output_file):
-    prompt = hub.pull("hwchase17/openai-tools-agent")
-    prompt.pretty_print()
+    prompt = hub.pull("hwchase17/react")
 
-    db = load_documents_db("llm_docs")
-    retriever = db.as_retriever()
+    file_tool = create_file_retrieval_tool()
 
 
-    file_tool = create_retriever_tool(
-        retriever,
-        "search_file_tools_docs",
-        """Searches and returns excerpts from documentation for CLI usage of the following tools:
-        - pandoc
-        - ffmpeg
-        - image magick
-
-        Search any query with the tool name to get the documentation for that tool. For example, "pandoc convert markdown to pdf" will return the documentation for the pandoc tool.
-        """,
-    )
-
-
-    execute_tool = Tool(name="Execute Shell Command",func=execute_command, description="Executes a shell command and returns the output.")
-
-
+    execute_tool = Tool(name="Execute Shell Command",func=execute_command, description="Executes a shell command and returns the output. Do not install any software or packages, all packages are available using tools in documentation")
 
 
     tools = [execute_tool, file_tool]
 
     # Construct the tool calling agent
-    agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, )
+    # agent = initialize_agent(tools, llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, callback_manager=callback_manager)
 
-    agent.handle_parsing_errors = True
-    # agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    agent = create_react_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, max_execution_time=25)
 
 
-    output = agent.invoke({
-        "input": f'{input} The output file should be: {output_file}',
+    output = agent_executor.invoke({
+        "input": f'{input}\nThe output file should be: {output_file}',
     })
 
     print(output)
     
 if __name__ == "__main__":
-    input = "downscale stick_figure.png to 10px by 10px and then add it three times to a pdf file and then convert that to docx"
+    input = "convert this latex file (math.tex) into a pdf ouptut"
 
-    init_tools_agent(llm_llama3, input, "stick_figure.docx")
+    init_tools_agent(llm_llama3, input, "mathoutput.pdf")
     # init_tools_agent(llm_llama3_8b, input, "llama8b.pdf")
     # init_tools_agent(llm_codellama, input, "codellama.png")
     # init_tools_agent(llm_mixtral22, input, "mixtral.png")
